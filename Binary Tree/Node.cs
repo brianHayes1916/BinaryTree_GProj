394b5b63f894123713b705f3c12d87761a11c262
Brian Hayes <briancampbellhayes@icloud.com>
2021-03-02 12:35:20 -0600

    Created the tree and node classes and gave node the member variable of int data will change to T.

---
+++ Binary Tree/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Tree
+{
+    class Node
+    {
+        public int data;
+
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+}
